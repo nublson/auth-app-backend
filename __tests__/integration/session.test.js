c5c0044a15399c2098f28d21efa8777d02dfe2a8
nubelsondev <nubelsondev@hotmail.com>
2020-03-08 22:25:17 +0000

    test: should be able to access private routes when authenticated

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -64,4 +64,20 @@ describe('Authentication', () => {
 
 		expect(response.body).toHaveProperty('token')
 	})
+
+	it('should be able to access private routes when authenticated', async () => {
+		const user = await factory.create('User')
+
+		const response = await request(app)
+			.get('/dashboard')
+			.set('Authorization', `Bearer ${user.generateToken()}`)
+
+		expect(response.status).toBe(200)
+	})
+
+	it('should not be able to access private routes without jwt token', async () => {
+		const response = await request(app).get('/dashboard')
+
+		expect(response.status).toBe(401)
+	})
 })
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 const routes = require('express').Router()
 
+const authMiddleware = require('./app/middleware/auth')
+
 const sessionController = require('./app/controllers/sessionController')
 
 routes.post('/sessions', sessionController.store)
+
+routes.use(authMiddleware)
+
+routes.get('/dashboard', (req, res) => {
+	return res.status(200).send()
+})
 
 module.exports = routes
