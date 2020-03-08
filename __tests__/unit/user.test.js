06911917e460277af0ef249e06240ea5863ee2f3
nubelsondev <nubelsondev@hotmail.com>
2020-03-08 19:44:30 +0000

    test: should encrypt user password

---
+++ __tests__/unit/user.test.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcryptjs')
+
+const truncate = require('../utils/truncate')
+const factory = require('../factories')
+
+describe('User', () => {
+	beforeEach(async () => {
+		await truncate()
+	})
+
+	it('Should encrypt user password', async () => {
+		const user = await factory.create('User', {
+			password: '123456'
+		})
+
+		const compareHash = await bcrypt.compare('123456', user.password_hash)
+
+		expect(compareHash).toBe(true)
+	})
+})
